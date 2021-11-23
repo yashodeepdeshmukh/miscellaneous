ac566821e0e84991fd2a911ed8a1e3b516be7023
Yashodeep Deshmukh <76196538+yashodeepdeshmukh@users.noreply.github.com>
2021-11-23 10:58:49 +0530

    Update data_path.py

--- data_path.py
+++ data_path.py
@@ -2,13 +2,13 @@ import os
 
 import pandas as pd
 
-os.chdir(r'C:\Users\yasho\Desktop\ML project\IITM\Driver genes\Dataset\Cell model and TCGA')
+os.chdir(r'PATH NAME')
 dir_path=os.getcwd()
-cell_model_path=os.path.join(dir_path,'Cell models')
-tcga_path=os.path.join(dir_path,'TCGA')
+FOLDERPATH1=os.path.join(dir_path,'FOLDERNAME1')
+FOLDERPATH2=os.path.join(dir_path,'FOLDERNAME2')
 datasets=[]
 #cell_model
-list1=os.listdir(cell_model_path)
+list1=os.listdir(FOLDERPATH1)
 file1={}
 for i in list1:
     n=''.join((i.split('.'))[:-1])
@@ -19,7 +19,7 @@ for key,val in file1.items():
         exec(key + '=val')
         datasets.append(key)
 #tcga_data
-list2=os.listdir(tcga_path)
+list2=os.listdir(FOLDERPATH2)
 file2={}
 for j in list2:
     m=''.join((j.split('.'))[:-1])
